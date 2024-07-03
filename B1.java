d3334a1f5d3c515059b68ed82ed36b2a4223b40d
Prethi14 <sprethigaa@gmail.com>
2024-07-03 13:20:00 +0530

    Files uploaded

---
+++ B1.java
@@ -0,0 +1,37 @@
+public class B1{
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 10.9f;
+        double c = 20.67;
+        String d = "good";
+        char e = 's';
+        System.out.println("**********************");
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println("***********************");
+        System.out.println(5+56+90); // 151
+        System.out.println("7"+19);  // 719
+        System.out.println(10+10+"9"); //209
+        System.out.println("9"+"8");   // 98
+        System.out.println("************************");
+        System.out.println("OPERATIONS");
+        int num1 =30;
+        int num2 =10;
+        int num3=5;
+        System.out.println(num1+num2); //40
+        System.out.println(num1-num2); //20
+        System.out.println(num1/num2); //3
+        System.out.println(num1%num2); //0
+        System.out.println("****************************");
+        System.out.println(num2>num1); //False
+        System.out.println(num1>num2); //True
+        System.out.println(num1>num2 || num1<num3); //True
+        System.out.println(num1<num2 && num1>num3); //False
+        
+        
+
+    }
+}
